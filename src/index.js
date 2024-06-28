3d8a6cd5169c147ae677002ecf3e0d3edc36ba2d
yaokun <122089402+TunArun@users.noreply.github.com>
2024-06-28 21:37:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tunarund.sbs": dockerHub,
+  "quay.tunarund.sbs": "https://quay.io",
+  "gcr.tunarund.sbs": "https://gcr.io",
+  "k8s-gcr.tunarund.sbs": "https://k8s.gcr.io",
+  "k8s.tunarund.sbs": "https://registry.k8s.io",
+  "ghcr.tunarund.sbs": "https://ghcr.io",
+  "cloudsmith.tunarund.sbs": "https://docker.cloudsmith.io",
+  "ecr.tunarund.sbs": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tunarund.sbs": dockerHub,
 };
 
 function routeByHosts(host) {
